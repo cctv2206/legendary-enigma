e35d6c4b3777befa0d5169d5f4515ee814ddb22b
Kai Kang <cctv2206@outlook.com>
2016-03-17 20:46:25 -0400

    Update validParentheses.cpp
    Added the problem description.

--- validParentheses.cpp
+++ validParentheses.cpp
@@ -1,3 +1,11 @@
+/*
+This is one of the Parentheses problems.
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*/
+
 class Solution {
 public:
     bool isValid(string s) {
