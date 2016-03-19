fc9f16cb12ea54f94ac1cfedd53d79e681c39ea4
Kai Kang <cctv2206@outlook.com>
2016-03-18 23:22:09 -0400

    Create findMinimumInRotatedArray.java

---
+++ findMinimumInRotatedArray.java
@@ -0,0 +1,39 @@
+/**
+One of the Array problems? Divide and conquer solution. Log(n). Similar to Binary Search.
+
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+*/
+
+public class Solution {
+    public int findMin(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        
+        int start = 0;
+        int end = length - 1;
+        
+        if (nums[start] <= nums[end]) { // not rotated
+            return nums[start];
+        }
+        
+        // binary search
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= nums[start]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return Math.min(nums[start], nums[end]);
+    }
+}
