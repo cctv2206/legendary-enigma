a145b463b68003a95a3f7e827697270246725476
Kai Kang <cctv2206@outlook.com>
2016-03-21 01:17:57 -0400

    Add another solution

--- permutation.java
+++ permutation.java
@@ -5,6 +5,8 @@ For example,
 [1,2,3] have the following permutations:
 [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
 */
+
+// generate permutations by growing
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
@@ -27,3 +29,36 @@ public class Solution {
         }
     }
 }
+
+// using swap array to generate permutations
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        permuteHelper(result, nums, 0);
+        return result;
+    }
+
+    public void permuteHelper(List<List<Integer>> result, int[] nums, int index) {
+        if (index == nums.length) {
+            List<Integer> oneSolution = new ArrayList<Integer>();
+            for (int num : nums) {
+                oneSolution.add(num);
+            }
+            result.add(oneSolution);
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            swap(index, i, nums);
+            permuteHelper(result, nums, index + 1);
+            swap(index, i, nums);
+        }
+    }
+
+    public void swap(int n, int m, int[] nums) {
+        int temp = nums[n];
+        nums[n] = nums[m];
+        nums[m] = temp;
+    }
+}
+
