ef7f362acdf267c754b25e8745566c1c73fd6d0b
Kai Kang <cctv2206@outlook.com>
2016-03-22 00:08:25 -0400

    Create combinationSum.java

---
+++ combinationSum.java
@@ -0,0 +1,42 @@
+/** 
+
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+A solution set is: 
+[7] 
+[2, 2, 3] 
+
+*/
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> solution = new ArrayList<Integer>();
+        Arrays.sort(candidates); // sort first!
+        combinationSumHelper(result, solution, candidates, target, 0);
+        return result;
+    }
+    
+    public void combinationSumHelper(List<List<Integer>> result, List<Integer> solution, int[] candidates, int target, int index) {
+        if (target == 0) {
+            result.add(solution);
+            return;
+        }
+        
+        for (int i = index; i < candidates.length; i++) {
+            List<Integer> temp = new ArrayList<Integer>(solution);
+            if (target - candidates[i] < 0) {
+                return;
+            }
+            temp.add(candidates[i]);
+            combinationSumHelper(result, temp, candidates, target - candidates[i], i);
+        }
+    }
+}
