c45d5f74e47ad980615c774adcf3e91441be620d
Kai Kang <cctv2206@outlook.com>
2016-03-28 00:33:18 -0400

    stupid solution

---
+++ spiralMatrixII.java
@@ -0,0 +1,67 @@
+/**
+
+Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+For example,
+Given n = 3,
+
+You should return the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
+
+*/
+
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        if (n <= 0) {
+            return result;
+        }
+        matrixHelper(result, 0, 0, n, 1, 3);
+        return result;
+    }
+    
+    public void matrixHelper(int[][] result, int i, int j, int n, int num, int direction) {
+        result[i][j] = num;
+        
+        if (num == n * n) { // over
+            return;
+        }
+        
+        switch (direction) {
+            case 1: // left
+                if (j - 1 >= 0 && result[i][j - 1] == 0) {
+                    matrixHelper(result, i, j - 1, n, num + 1, 1);
+                } else if (i - 1 >= 0 && result[i - 1][j] == 0) {
+                    matrixHelper(result, i - 1, j, n, num + 1, 2);
+                }
+                return;
+            case 2: // up
+                if (i - 1 >= 0 && result[i - 1][j] == 0) {
+                    matrixHelper(result, i - 1, j, n, num + 1, 2);
+                } else if (j + 1 < n && result[i][j + 1] == 0) {
+                    matrixHelper(result, i, j + 1, n, num + 1, 3);
+                }
+                return;
+            case 3: // right
+                if (j + 1 < n && result[i][j + 1] == 0) {
+                    matrixHelper(result, i, j + 1, n, num + 1, 3);
+                } else if (i + 1 < n && result[i + 1][j] == 0) {
+                    matrixHelper(result, i + 1, j, n, num + 1, 4);
+                }
+                return;
+            case 4: // down
+                if (i + 1 < n && result[i + 1][j] == 0) {
+                    matrixHelper(result, i + 1, j, n, num + 1, 4);
+                } else if (j - 1 >= 0 && result[i][j - 1] == 0) {
+                    matrixHelper(result, i, j - 1, n, num + 1, 1);
+                }
+                return;
+        }
+        
+    }
+}
