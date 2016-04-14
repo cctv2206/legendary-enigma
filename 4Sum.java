5674c5676e11e0a1e1598da5b60035e33a9544b6
Kai Kang <cctv2206@outlook.com>
2016-04-14 11:09:47 -0400

    3 sum solution

---
+++ 4Sum.java
@@ -0,0 +1,58 @@
+/**
+
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+The solution set must not contain duplicate quadruplets.
+
+For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+
+A solution set is:
+(-1,  0, 0, 1)
+(-2, -1, 1, 2)
+(-2,  0, 0, 2)
+
+*/
+
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int threeSum = target - nums[i];
+
+            // three sum
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1;
+                int l = length - 1;
+                while (k < l) {
+                    int sum = nums[j] + nums[k] + nums[l];
+                    if (sum == threeSum) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    } else if (sum < threeSum) {
+                        k++;
+                        // while (k < l && nums[k] == nums[k - 1]) k++;
+                    } else { // sum > threeSum
+                        l--;
+                        // while (k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                    
+                } // end while k < l
+                
+            } // end for j
+
+        } // end for i
+
+        return result;
+    }
+}
