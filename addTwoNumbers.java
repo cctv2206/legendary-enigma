3270963eba834d8e55552d61d8d9943b12ec7c31
Kai Kang <cctv2206@outlook.com>
2016-04-20 15:29:20 -0400

    add extra space solution

--- addTwoNumbers.java
+++ addTwoNumbers.java
@@ -66,3 +66,26 @@ public class Solution {
         return node;
     }
 }
+
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        
+        int carry = 0;
+        int sum;
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        
+        return dummy.next;
+    }
+}
