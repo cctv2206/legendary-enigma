5d131847371a7ef831c0dc2ff62dbaaea9f63904
Kai Kang <cctv2206@outlook.com>
2016-04-19 22:37:08 -0400

    Merge sort

---
+++ sortList.java
@@ -0,0 +1,77 @@
+/**
+
+Sort a linked list in O(n log n) time using constant space complexity.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+// cannot use insertion sort
+// use merge sort
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        // break list
+        ListNode head2 = middle(head);
+        
+        return merge(sortList(head), sortList(head2));
+    }
+    
+    
+    // merge two list, return head
+    public ListNode merge(ListNode node1, ListNode node2) {
+        if (node1 == null) {
+            return node2;
+        } else if (node2 == null) {
+            return node1;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        
+        while (node1 != null && node2 != null) {
+            if (node1.val < node2.val) {
+                cur.next = node1;
+                node1 = node1.next;
+            } else {
+                cur.next = node2;
+                node2 = node2.next;
+            }
+            cur = cur.next;
+        }
+        
+        if (node1 != null) {
+            cur.next = node1;
+        } else {
+            cur.next = node2;
+        }
+        
+        return dummy.next;
+    }
+    
+    // break the list, return the middle
+    public ListNode middle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode tail = new ListNode(0);
+        tail.next = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            tail = tail.next;
+        }
+        
+        tail.next = null;
+        return slow;
+    }
+}
