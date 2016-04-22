f96e80513873cde883e97d888c9f17f9482a419f
Kai Kang <cctv2206@outlook.com>
2016-04-22 01:15:59 -0400

    Create simplifyPath.java

---
+++ simplifyPath.java
@@ -0,0 +1,58 @@
+/**
+
+Given an absolute path for a file (Unix-style), simplify it.
+
+For example,
+path = "/home/", => "/home"
+path = "/a/./b/../../c/", => "/c"
+click to show corner cases.
+
+Corner Cases:
+Did you consider the case where path = "/../"?
+In this case, you should return "/".
+Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+In this case, you should ignore redundant slashes and return "/home/foo".
+
+*/
+
+public class Solution {
+    public String simplifyPath(String path) {
+        char[] charArray = path.toCharArray();
+        List<String> newPath = new ArrayList<String>();
+        int length = path.length();
+        String filename = "";
+        int start;
+        
+        for (int i = 0; i < length; i++) {
+            if (charArray[i] == '/' ) {
+                continue;
+            }
+            
+            start = i;
+            while (i < length && charArray[i] != '/') {
+                i++;
+            }
+            filename = path.substring(start, i);
+            
+            if (filename.equals("..")) { // name = ..
+                if (newPath.size() != 0) {
+                    newPath.remove(newPath.size() - 1);
+                }
+            } else if (!filename.equals(".")) { // some name
+                newPath.add(filename);
+                filename = "";
+            }
+        }
+        
+        if (newPath.size() == 0) {
+            return "/";
+        }
+        
+        StringBuilder res = new StringBuilder();
+        for (String name : newPath) {
+            res.append("/").append(name);
+        }
+        
+        return res.toString();
+    }
+}
