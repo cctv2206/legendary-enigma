13838b40c34ae2762c79824c0fd0e17952726d06
Kai Kang <cctv2206@outlook.com>
2016-06-04 15:16:30 -0500

    Bucket Sort Solution

---
+++ topKFrequentElements.java
@@ -0,0 +1,49 @@
+/**
+
+Given a non-empty array of integers, return the k most frequent elements.
+
+For example,
+Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+*/
+
+
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        
+        Map<Integer, Integer> numToCount = new HashMap<Integer, Integer>();
+        
+        for (int num : nums) {
+            if (numToCount.containsKey(num)) {
+                numToCount.put(num, numToCount.get(num) + 1);
+            } else {
+                numToCount.put(num, 1);
+            }
+        }
+        
+        List<Integer> [] counts = new ArrayList [nums.length + 1];
+        
+        for (int num : numToCount.keySet()) {
+            int count = numToCount.get(num);
+            if (counts[count] == null) counts[count] = new ArrayList();
+            counts[count].add(num);
+        }
+        
+        List<Integer> res = new ArrayList<Integer>();
+        
+        for (int i = nums.length, j = 0; i > 0 && k > 0; k--) {
+            while (counts[i] == null || j == counts[i].size()) {
+                i--;
+                j = 0;
+            }
+            res.add(counts[i].get(j));
+            j++;
+        }
+        
+        return res;
+    }
+}
