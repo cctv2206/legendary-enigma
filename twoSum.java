ae27e756083e02a80e7a3461b90f525fea04a4b7
Kai Kang <cctv2206@outlook.com>
2016-06-06 19:36:36 -0500

    Add Sort & two pointers solution

--- twoSum.java
+++ twoSum.java
@@ -30,3 +30,47 @@ public class Solution {
 
     }
 }
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] nums2 = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(nums2);
+        
+        int left = 0;
+        int right = nums2.length - 1;
+        
+        while (left + 1 < right) {
+            if (nums2[left] + nums2[right] == target) break;
+            
+            if (nums2[left] + nums2[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        int[] res = new int [2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums2[left]) {
+                res[0] = i;
+            }
+        }
+        
+        if (nums2[left] != nums2[right]) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == nums2[right]) {
+                    res[1] = i;
+                }
+            }
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == nums2[right] && i != res[0]) {
+                    res[1] = i;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
