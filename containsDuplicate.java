05547a557120b9097e4a1c61e106b69c5767a8ed
Kai Kang <cctv2206@outlook.com>
2016-06-16 15:41:20 -0500

    Add a solution using sorting
    How come sorting is faster than HashSet?

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -17,3 +17,16 @@ public class Solution {
     }
 }
 
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) return true;
+        }
+        
+        return false;
+    }
+}
