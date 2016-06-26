70ce3cd07b6f270e69244bef674d8e986c2df093
Kai Kang <cctv2206@outlook.com>
2016-06-26 15:26:39 -0500

    Update missingNumber.java

--- missingNumber.java
+++ missingNumber.java
@@ -18,3 +18,13 @@ public class Solution {
         return res;
     }
 }
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res += i - nums[i];
+        }
+        return res + nums.length;
+    }
+}
