9612a9f146b5b30150e99964fa437dc53f0842bc
Kai Kang <cctv2206@outlook.com>
2016-07-07 16:32:23 -0500

    better solution, one pass

--- plusOne.java
+++ plusOne.java
@@ -29,3 +29,25 @@ public class Solution {
     }
     
 }
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                break;
+            }
+        }
+        
+        if (digits[0] != 0) {
+            return digits;
+        }
+        
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+    
+}
