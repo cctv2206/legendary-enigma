53fc3875457a005814858b5590b287f38c0910bd
Kai Kang <cctv2206@outlook.com>
2016-07-26 16:05:47 -0500

    without StringBuilder using for loop

--- summaryRanges.java
+++ summaryRanges.java
@@ -28,3 +28,20 @@ public class Solution {
         return res;
     }
 }
+
+
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<String>();
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            while (i + 1 < nums.length && (nums[i + 1] - nums[i]) == 1) i++;
+            if (start == nums[i]) {
+                res.add(start + "");
+            } else {
+                res.add(start + "->" + nums[i]);
+            }
+        }
+        return res;
+    }
+}
