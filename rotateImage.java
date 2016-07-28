8e31f4e0a9ffa1737c27afbfc65147afd62a8883
Kai Kang <cctv2206@outlook.com>
2016-07-28 17:36:18 -0500

    solution with two for loop

--- rotateImage.java
+++ rotateImage.java
@@ -30,3 +30,23 @@ public class Solution {
         matrix[k][h] = temp;
     }
 }
+
+
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix.length == 0) return;
+        
+        int length = matrix.length;
+        
+        for (int i = 0; i < length / 2; i++) {
+            for (int j = i; j < length - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[length - 1 - j][i];
+                matrix[length - 1 - j][i] = matrix[length - 1 - i][length - 1 - j];
+                matrix[length - 1 - i][length - 1 - j] = matrix[j][length - 1 - i];
+                matrix[j][length - 1 - i] = temp;
+            }
+        }
+    }
+}
