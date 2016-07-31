ddd4b4cdeadd06b30f8a1403d36228e6ab3bb617
Kai Kang <cctv2206@outlook.com>
2016-07-31 13:39:50 -0500

    one-pass solution

--- sortColors.java
+++ sortColors.java
@@ -51,3 +51,31 @@ public class Solution {
         
     }
 }
+
+
+public class Solution {
+    public void sortColors(int[] nums) {
+        int zero = 0;
+        int two = nums.length - 1;
+        int i = 0;
+        while (i <= two) {
+            if (nums[i] == 0) {
+                swap(nums, i, zero);
+                zero++;
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, two);
+                two--;
+            } else {
+                i++;
+            }
+        }
+        
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
