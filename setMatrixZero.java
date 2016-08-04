3956959cff8328f2d85b3855e5c86d5eb45b2b25
Kai Kang <cctv2206@outlook.com>
2016-08-04 14:21:35 -0500

    better solution, two scans

--- setMatrixZero.java
+++ setMatrixZero.java
@@ -79,3 +79,36 @@ public class Solution {
     
     }
 }
+
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int firstCol = 1;
+        
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstCol = 0;
+            }
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j > 0; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (firstCol == 0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
