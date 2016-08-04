ec24677f3ac281b4cb26d369feec9a952f7c379c
Kai Kang <cctv2206@outlook.com>
2016-08-04 15:02:55 -0500

    BinarySearch Solution

---
+++ Search2DMatrix.java
@@ -0,0 +1,69 @@
+/**
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+For example,
+
+Consider the following matrix:
+
+[
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+
+Given target = 3, return true.
+*/
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 && matrix[0].length == 0) return false;
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int start = 0;
+        int end = rows - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[mid][0] > target) {
+                end = mid;
+            } else if (matrix[mid][0] < target) {
+                start = mid;
+            } else {
+                return true;
+            }
+        }
+        
+        if (matrix[start][0] > target) {
+            return false;
+        }
+        int row = start;
+        if (matrix[end][0] <= target) {
+            row = end;
+        }
+        
+        start = 0;
+        end = cols - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[row][mid] > target) {
+                end = mid;
+            } else if (matrix[row][mid] < target) {
+                start = mid;
+            } else {
+                return true;
+            }
+        }
+        
+        if (matrix[row][start] == target || matrix[row][end] == target) {
+            return true;
+        }
+        
+        return false;
+        
+    }
+}
