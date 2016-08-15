232a326aece312e8b76417934d5a8e5480f6eb0c
Kai Kang <cctv2206@outlook.com>
2016-08-15 15:41:14 -0500

    Update findMinimumInRotatedArray.java

--- findMinimumInRotatedArray.java
+++ findMinimumInRotatedArray.java
@@ -37,3 +37,25 @@ public class Solution {
         return Math.min(nums[start], nums[end]);
     }
 }
+
+public class Solution {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        if (nums[start] < nums[end]) {
+            return nums[start];
+        }
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[start]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return nums[start] < nums[end] ? nums[start] : nums[end];
+    }
+}
