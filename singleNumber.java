81bf4e34c9cf517fc632a7dc0d5894d6d233aa6b
Kai Kang <cctv2206@outlook.com>
2016-09-16 21:31:48 -0500

    Update singleNumber.java

--- singleNumber.java
+++ singleNumber.java
@@ -7,6 +7,16 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 
 public class Solution {
     public int singleNumber(int[] nums) {
+        int num = 0;
+        for (int i = 0; i < nums.length; i++) {
+            num ^= nums[i];
+        }
+        return num;
+    }
+}
+
+public class Solution {
+    public int singleNumber(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
