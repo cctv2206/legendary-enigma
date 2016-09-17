51b50c15acca4a577b8e253cebc3783bd450aec9
Kai Kang <cctv2206@outlook.com>
2016-09-16 21:47:11 -0500

    XOR

--- FindTheDifference.java
+++ FindTheDifference.java
@@ -21,6 +21,18 @@ Explanation:
 
 public class Solution {
     public char findTheDifference(String s, String t) {
+        char c = 0;
+        c ^= t.charAt(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            c ^= s.charAt(i);
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
+}
+
+public class Solution {
+    public char findTheDifference(String s, String t) {
         int res = 0;
         for (int i = 0; i < s.length(); i++) {
             res ^= s.charAt(i) - 'a';
