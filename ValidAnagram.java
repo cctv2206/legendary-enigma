322d0d0d9fd6ace46501400a8cba8eb17236fce9
Kai Kang <cctv2206@outlook.com>
2016-09-18 16:25:28 -0500

    Create ValidAnagram.java

---
+++ ValidAnagram.java
@@ -0,0 +1,41 @@
+/**
+
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+*/
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] chars = new int[26];
+        
+        for (int i = 0; i < s.length(); i++) {
+            chars[s.charAt(i) - 'a']++;
+        }
+        
+        for (int i = 0; i < t.length(); i++) {
+            chars[t.charAt(i) - 'a']--;
+        }
+        
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
