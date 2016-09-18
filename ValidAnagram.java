fe80f5dbcec4511b2a9e66a4968f978efa7cdf2d
Kai Kang <cctv2206@outlook.com>
2016-09-18 16:31:18 -0500

    add a solution using sort

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -39,3 +39,20 @@ public class Solution {
         return true;
     }
 }
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        
+        
+        return Arrays.equals(sArr, tArr);
+    }
+}
