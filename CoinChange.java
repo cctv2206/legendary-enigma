105966b92d45aea0350042f83bcdc667f1fdd6eb
Kai Kang <cctv2206@outlook.com>
2016-11-02 20:21:45 -0500

    DP solution 2

--- CoinChange.java
+++ CoinChange.java
@@ -16,6 +16,25 @@ You may assume that you have an infinite number of each kind of coin.
 
 public class Solution {
     public int coinChange(int[] coins, int amount) {
+        if (amount < 1) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                if (dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
+
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
